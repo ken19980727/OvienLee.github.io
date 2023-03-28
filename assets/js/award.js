a1158729f884ceb32149d137f4f861a75644a5c7
ken19980727 <ken19980727@gmail.com>
2023-03-29 00:46:28 +0800

    modified the section3 content

--- assets/js/award.js
+++ assets/js/award.js
@@ -4,13 +4,14 @@ const sec3_1 = new Vue({
         award_category : "AIGO",
         awards: [{
                 title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「利用情感語意分析建構商家網路評價機制」-佳作',
-                simple_text: ['Establish an algorithm/system to filter malicious online reviews of merchants and design a brand-new rating mechanism that quantifies the score more accurately, thus enhancing the objectivity and reliability of the evaluations.'],
+                simple_text: ['全新評論的評分機制',
+                              '設計一套清洗濫充評論流程'],
             },
             {
                 title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「原住民語音生成」-特優',
-                simple_text: ['Use AI to input an indigenous sentence and generate the corresponding sound to achieve the effect of Text to Speech (TTS) on the front-end webpage of the local setting.',
-                	'Use Tacotron and Tacotron2 (improved version) as the TTS model.' , 
-                	'The total length of the sentence is 6 hours, with sentence intervals falling within 4sec~10sec, and the Log-Spectral Distance is less than 15.']
+                simple_text: ['Design an aborigines Text to Speech (TTS)',
+                	           'Use Tacotron and Tacotron2 as the TTS.' , 
+                	           'Log-Spectral Distance is less than 15']
             },
         ]
     }
@@ -21,14 +22,34 @@ const sec3_2 = new Vue({
     data: {
         award_category : "HACKATHON",
         awards: [{
-                title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「利用情感語意分析建構商家網路評價機制」-佳作',
-                simple_text: ['Establish an algorithm/system to filter malicious online reviews of merchants and design a brand-new rating mechanism that quantifies the score more accurately, thus enhancing the objectivity and reliability of the evaluations.'],
+                title: '法律法遵黑客松-法遵特別獎',
+                simple_text: ['爬取法規與相關案件',
+                            'Hierarchical Attention Networks for MultiLabel Law-Document Classification',
+                            ],
             },
             {
-                title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「原住民語音生成」-特優',
-                simple_text: ['Use AI to input an indigenous sentence and generate the corresponding sound to achieve the effect of Text to Speech (TTS) on the front-end webpage of the local setting.',
-                    'Use Tacotron and Tacotron2 (improved version) as the TTS model.' , 
-                    'The total length of the sentence is 6 hours, with sentence intervals falling within 4sec~10sec, and the Log-Spectral Distance is less than 15.']
+                title: '保險金融與AI智慧健康區塊鏈 黑客松競賽-佳作',
+                simple_text: ['以台北醫學大學提供之資料',
+                            "完成一套ICU病床分配系統"]
+            },
+        ]
+    }
+})
+
+const sec3_3 = new Vue({
+    el: '#hackathon',
+    data: {
+        award_category : "AICUP",
+        awards: [{
+                title: '法律法遵黑客松-法遵特別獎',
+                simple_text: ['爬取法規與相關案件',
+                            'Hierarchical Attention Networks for MultiLabel Law-Document Classification',
+                            ],
+            },
+            {
+                title: '保險金融與AI智慧健康區塊鏈 黑客松競賽-佳作',
+                simple_text: ['以台北醫學大學提供之資料',
+                            "完成一套ICU病床分配系統"]
             },
         ]
     }
