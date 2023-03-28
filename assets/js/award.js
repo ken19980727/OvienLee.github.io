a36eba41dce8be5e6a542219f20f3d8e29f2d186
ovien_lee <ovien_lee@asus.com>
2023-03-28 20:26:12 +0800

    sss

---
+++ assets/js/award.js
@@ -0,0 +1,35 @@
+const sec3_1 = new Vue({
+    el: '#aigo',
+    data: {
+        award_category : "AIGO",
+        awards: [{
+                title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「利用情感語意分析建構商家網路評價機制」-佳作',
+                simple_text: ['Establish an algorithm/system to filter malicious online reviews of merchants and design a brand-new rating mechanism that quantifies the score more accurately, thus enhancing the objectivity and reliability of the evaluations.'],
+            },
+            {
+                title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「原住民語音生成」-特優',
+                simple_text: ['Use AI to input an indigenous sentence and generate the corresponding sound to achieve the effect of Text to Speech (TTS) on the front-end webpage of the local setting.',
+                	'Use Tacotron and Tacotron2 (improved version) as the TTS model.' , 
+                	'The total length of the sentence is 6 hours, with sentence intervals falling within 4sec~10sec, and the Log-Spectral Distance is less than 15.']
+            },
+        ]
+    }
+})
+
+const sec3_2 = new Vue({
+    el: '#hackathon',
+    data: {
+        award_category : "HACKATHON",
+        awards: [{
+                title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「利用情感語意分析建構商家網路評價機制」-佳作',
+                simple_text: ['Establish an algorithm/system to filter malicious online reviews of merchants and design a brand-new rating mechanism that quantifies the score more accurately, thus enhancing the objectivity and reliability of the evaluations.'],
+            },
+            {
+                title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「原住民語音生成」-特優',
+                simple_text: ['Use AI to input an indigenous sentence and generate the corresponding sound to achieve the effect of Text to Speech (TTS) on the front-end webpage of the local setting.',
+                    'Use Tacotron and Tacotron2 (improved version) as the TTS model.' , 
+                    'The total length of the sentence is 6 hours, with sentence intervals falling within 4sec~10sec, and the Log-Spectral Distance is less than 15.']
+            },
+        ]
+    }
+})
