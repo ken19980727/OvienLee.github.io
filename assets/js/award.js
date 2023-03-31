55339942a0141b7205302acb7182ce16b2f12ad3
ovien_lee <ovien_lee@asus.com>
2023-03-31 17:34:43 +0800

    correct section3 date

--- assets/js/award.js
+++ assets/js/award.js
@@ -6,14 +6,16 @@ const sec3_1 = new Vue({
                 title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「利用情感語意分析建構商家網路評價機制」-佳作',
                 simple_text: ['全新商家評論的評分機制',
                               '提供一套可行 POC 方案，有效過濾濫充評論'],
-                date:'mm yy'
+                date:'Dec 2022',
+                image : "images/awards/award_test.jpg"
             },
             {
                 title: '經濟部工業局-AI 產業實戰應用人才淬煉計畫(AIGO)「原住民語音生成」-特優',
                 simple_text: ['開發原住民文字轉語音',
                 	           'Tacotron and Tacotron2 作為模型架構並比較' , 
                 	           'Log-Spectral Distance 小於 15'],
-                date:'mm yy'
+                date:'Oct 2021',
+                image : "images/awards/aborigines.jpg"
             },
         ]
     }
@@ -27,13 +29,13 @@ const sec3_2 = new Vue({
                 title: '法律法遵黑客松-法遵特別獎',
                 simple_text: ['以代理法遵完成案件法規多標籤分類',
                             '設計分層式注意力模型達到可解釋性 AI'],
-                date:'mm yy'
+                date:'Aug 2021'
             },
             {
                 title: '保險金融與AI智慧健康區塊鏈 黑客松競賽-佳作',
                 simple_text: ['以台北醫學大學提供之資料',
                             "開發一套ICU病床分配系統"],
-                date:'mm yy'
+                date:'July 2019'
             },
         ]
     }
@@ -49,14 +51,14 @@ const sec3_3 = new Vue({
                             '基於 Sentence piece RoBERta 作為模型結合 CRF 進行微調',
                             '使用正規表示法改善隱私欄位歧異現象',
                             ],
-                date:'mm yy'
+                date:'Dec 2020'
             },
             {
                 title: '人工智慧論文機器閱讀競賽之論文標註-前標 (Ranking : 33/531)',
                 simple_text: ['給定 Computer Science 相關的論文摘要，對每個句子進行分類(Background、Objectives、Methods、Results、Conclusions、Others)',
                             "同時使用 Glove & BERT 作為 word embedding",
                             "比較 GRU 與 LSTM 作為 neural networks 效能差異"],
-                date:'mm yy'
+                date:'Dec 2019'
             },
         ]
     }
@@ -68,23 +70,20 @@ const sec3_4 = new Vue({
         award_category : "OTHERS",
         awards: [{
                 title: 'Hahow-上架線上課程「學習AI一把抓：點亮人工智慧技能樹」',
-                simple_text: ['去識別醫生與病人的談話之間的隱私欄位',
-                            '基於 Sentence piece RoBERta 作為模型結合 CRF 進行微調',
-                            '使用正規表示法改善隱私欄位歧異現象',
-                            ],
-                date:'mm yy'
+                simple_text: ['Python Online IDE 操作',
+                            '資料分析與特徵工程',
+                            '機器學習與類神經網路教學'],
+                date:'Apr 2022'
             },
             {
                 title: 'Intel OpenVINO™ x Edge AI創意應用競賽-佳作',
-                simple_text: ['給定 Computer Science 相關的論文摘要，對每個句子進行分類(Background、Objectives、Methods、Results、Conclusions、Others)',
-                            "同時使用 Glove & BERT 作為 word embedding",
-                            "比較 GRU 與 LSTM 作為 neural networks 效能差異"],
-                date:'mm yy'
+                simple_text: ['使用 OpenVINO toolkit 完成 Object Detection'],
+                date:'Nov 2019'
             },
             {
                 title: '2018全國大專院校羽球比賽大電盃-冠軍',
                 simple_text: [''],
-                date:'mm yy'
+                date:'Nov 2018'
             },
         ]
     }
